164f85d7130b3c4d2b539d8eba6b29c3011ad576
Dean Grande <dean.grande@twobridges.com.au>
2014-03-19 11:33:53 +1100

    initial

---
+++ tb.docs/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="tb.docs.MvcApplication" Language="C#" %>
---
+++ tb.docs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tb.docs.Startup))]
+namespace tb.docs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
